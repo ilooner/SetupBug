fcb72225c9e60e2b06aba9266e8d1002231d36ef
Timothy Farkas <tim@datatorrent.com>
2015-08-20 14:40:14 -0700

    Finished test app

--- src/main/java/com/datatorrent/setupbug/Application.java
+++ src/main/java/com/datatorrent/setupbug/Application.java
@@ -3,13 +3,17 @@
  */
 package com.datatorrent.setupbug;
 
-import org.apache.hadoop.conf.Configuration;
-
-import com.datatorrent.api.annotation.ApplicationAnnotation;
-import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.Context.OperatorContext;
 import com.datatorrent.api.DAG;
 import com.datatorrent.api.DAG.Locality;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.Operator;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.annotation.ApplicationAnnotation;
 import com.datatorrent.lib.io.ConsoleOutputOperator;
+import org.apache.hadoop.conf.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationAnnotation(name="MyFirstApplication")
 public class Application implements StreamingApplication
@@ -23,9 +27,50 @@ public class Application implements StreamingApplication
 
     RandomNumberGenerator randomGenerator = dag.addOperator("randomGenerator", RandomNumberGenerator.class);
     randomGenerator.setNumTuples(500);
+    SetupOperator setupOperator = dag.addOperator("SetupOperator", SetupOperator.class);
 
-    ConsoleOutputOperator cons = dag.addOperator("console", new ConsoleOutputOperator());
+    dag.addStream("randomData", randomGenerator.out, setupOperator.inputPort);
+  }
 
-    dag.addStream("randomData", randomGenerator.out, cons.input).setLocality(Locality.CONTAINER_LOCAL);
+  public static class SetupOperator implements Operator
+  {
+    private transient boolean setupCalled = false;
+
+    public final DefaultInputPort<Double> inputPort = new DefaultInputPort<Double>() {
+      @Override
+      public void process(Double t)
+      {
+        if(!setupCalled) {
+          LOG.info("Setup called.");
+        }
+      }
+    };
+
+    public SetupOperator()
+    {
+    }
+
+    @Override
+    public void setup(OperatorContext cntxt)
+    {
+      setupCalled = true;
+    }
+
+    @Override
+    public void beginWindow(long l)
+    {
+    }
+
+    @Override
+    public void endWindow()
+    {
+    }
+
+    @Override
+    public void teardown()
+    {
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(SetupOperator.class);
   }
 }
